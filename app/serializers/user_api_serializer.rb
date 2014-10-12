4001eba310b197c3de3109d131a90f86ab91e2a4
Stephen Bussey <steve.bussey@salesloft.com>
2014-10-12 10:49:33 -0700

    better protection when SFDC sync fails

--- app/serializers/user_api_serializer.rb
+++ app/serializers/user_api_serializer.rb
@@ -1,5 +1,9 @@
 class UserApiSerializer < ActiveModel::Serializer
-  attributes :id, :name, :organization, :email, :sfdc_config, :sfdc_setup
+  attributes :id, :name, :organization, :email, :sfdc_config, :sfdc_setup, :sfdc_keys
+
+  def sfdc_keys
+    object.sfdc_oauth_token.present?
+  end
 
   def sfdc_config
     object.normalized_sfdc_config
